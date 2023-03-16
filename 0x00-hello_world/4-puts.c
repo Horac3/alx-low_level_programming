f7b30ad9b2dedb225ade4009a03fdbe234df8509
Horac3 <lwandahorace@gmail.com>
2023-03-16 09:37:24 -0700

    Hello C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char MyChar[] = "Programming is like building a multilingual puzzle\n";
+    puts(MyChar);
+    return 0;
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+Variour C programs
