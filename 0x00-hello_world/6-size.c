f1ce1fda4ef3710184c990201d61d80f2216f403
Horac3 <lwandahorace@gmail.com>
2023-03-16 10:54:21 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ char charType;
 long int longIntType;
 long long int longLongIntType;
 
-printf("Size of a char: %zu byte\n", sizeof(charType));
-printf("Size of an int: %zu bytes\n", sizeof(intType));
-printf("Size of a long int: %zu bytes\n", sizeof(longIntType));
-printf("Size of a long long int: %zu bytes\n", sizeof(longLongIntType));
-printf("Size of a float: %zu bytes\n", sizeof(floatType));
+printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
+printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
 
 return (0);
 }
