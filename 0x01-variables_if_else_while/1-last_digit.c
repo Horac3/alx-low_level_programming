171a503cd814da667bd9a3017b317a513b0ab5bf
Horac3 <lwandahorace@gmail.com>
2023-03-17 14:38:38 -0700

    Fixed declaration in task 2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,8 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-int last_digit = abs(n) % 10;
+int last_digit; 
+last_digit = abs(n) % 10;
 printf("The last digit of %d is %d\n", n, last_digit);
 return (0);
 }
