837e532d3e58a1c1f96a7d41bc65cea7fe2b86a6
Horac3 <lwandahorace@gmail.com>
2023-03-21 09:23:45 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - C program that prints with last digit of an int
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Today is all about functions and nested loops. Files in this directory are a reflection of my understanding of each concept
