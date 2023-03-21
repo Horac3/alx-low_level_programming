b0a1f674c71649796aace33b1ea7389c88a0f554
Horac3 <lwandahorace@gmail.com>
2023-03-21 13:21:40 -0700

    Files 2-11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - addition of two integers
+* @x: number to be added
+* @y: number to be added
+* Return: sum of x and y
+*/
+int add(int x, int y)
+{
+int z;
+z = x + y;
+return (z);
+}
+
