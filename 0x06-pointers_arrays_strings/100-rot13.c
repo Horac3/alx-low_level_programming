74da1f972c0c54203b2630a4802b3babae293001
Horace__ <85098623+Horac3@users.noreply.github.com>
2023-04-01 08:00:16 +0200

    Worked on a few files

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -11,15 +11,15 @@ char *rot13(char *s)
 {
 int i;
 int j;
-char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+char str[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char strot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; j < 52; j++)
 {
-if (s[i] == data1[j])
+if (s[i] == str[j])
 {
-s[i] = datarot[j];
+s[i] = strot[j];
 break;
 }
 }
