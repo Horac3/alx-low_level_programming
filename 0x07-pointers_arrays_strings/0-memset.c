41b58b03faa1165e448aee5a5ed3c9c863e1c836
Horac3 <lwandahorace@gmail.com>
2023-04-03 11:16:40 -0700

    Initial Commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - Write a function that fills memory with a constant byte.
+ * @s: memory address to be filled
+ * @b: the desired value
+ * @n: bytes of memory
+ * Return: value for n bytes
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int mem_size;
+
+	mem_size = 0;
+	while (x < mem_size)
+	{
+	s[mem_size++] = b;
+	}
+	return (s);
+}
