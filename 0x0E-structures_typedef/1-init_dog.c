37c1178b4963b75ccffcf242529892ac80050ec4
Horac3 <lwandahorace@gmail.com>
2023-04-17 10:15:46 -0700

    typedef and struct well understood

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = malloc(strlen(name) + 1);
+	strcpy(d->name, name);
+	d->age = age;
+	d->owner = malloc(strlen(owner) + 1);
+	strcpy(d->owner, owner);
+}
+
