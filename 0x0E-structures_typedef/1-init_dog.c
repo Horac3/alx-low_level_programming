d8f7533e095870bfb30c7e9325d535a40f71111f
Horac3 <lwandahorace@gmail.com>
2023-04-17 11:10:26 -0700

    1-init_dog worked on

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,10 +14,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = malloc(strlen(name) + 1);
-	strcpy(d->name, name);
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
 	d->age = age;
-	d->owner = malloc(strlen(owner) + 1);
-	strcpy(d->owner, owner);
+	d->owner = owner;
 }
 
