b2e68a6670af69701090cc652928bf110c423b6c
Horac3 <lwandahorace@gmail.com>
2023-05-02 06:37:58 -0700

    printlist_int

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,6 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
+
 /**
  * print_listint - prints all the elements of a listint_t list
  * @h: pointer to the head of the list
@@ -10,7 +11,7 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		count++;
